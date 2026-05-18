7b7298fa7c091e2a0dc9a1803058bd1455b3d76d
Abrar <140312789+0x00ctrl@users.noreply.github.com>
2026-05-18 18:17:10 +0500

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,38 @@
 if status is-interactive
-	starship init fish | source
+    # Commands to run in interactive sessions can go here
 end
+set -U fish_greeting
+
+function fish_prompt
+    # Set the color to green for the arrow
+    set_color green
+    echo -n "➜ "
+    # Reset color for the command input
+    set_color normal
+end
+
+alias c="clear"
+alias mm="micro"
+alias cd..="cd .."
+alias curl="curl --user-agent 'noleak'"
+alias l="ls -ahls --color=auto"
+alias r="reset"
+alias shred="shred -zf"
+alias sl="ls --color=auto"
+alias vi="vim"
+alias ls="ls --color=auto"
+alias dir="dir --color=auto"
+
+alias vdir="vdir --color=auto"
+alias grep="grep --color=auto"
+alias fgrep="fgrep --color=auto"
+alias egrep="egrep --color=auto"
+alias wget="wget -c --user-agent 'noleak'"
+alias dd="dd status=progress"
+alias cp="cp -i"                          # confirm before overwriting something
+alias rm="rm -i"
+alias mv="mv -i"
+alias df="df -h"                          # human-readable sizes
+alias free="free -h"
+alias du="du -h"
+starship init fish | source
